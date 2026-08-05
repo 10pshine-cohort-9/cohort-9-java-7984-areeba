16aa315dd7808a54955efe4a225a95aaea2ea083
Areeba <areebaqamar2901@gmail.com>
2026-08-05 15:19:42 +0500

    refactor: rename base package to com.tenpearls.contactmanagement

--- contact-management-system/src/main/java/com/tenpearls/contactmanagement/ContactManagementSystemApplication.java
+++ contact-management-system/src/main/java/com/tenpearls/contactmanagement/ContactManagementSystemApplication.java
@@ -1,4 +1,4 @@
-package com.example.contact_management_system;
+package com.tenpearls.contactmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
